6a73783fde76d054b0e51b4a9e9a36e0813df994
azure with devops <darshan.az@outlook.com>
2026-05-04 02:01:44 +0530

    established a simple API call from frontend to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import React, { useEffect } from "react";
+
 function App() {
+
+  useEffect(() => {
+    fetch("http://localhost:8000/health")
+      .then(res => res.json())
+      .then(data => console.log("Backend Response:", data))
+      .catch(err => console.error("Error:", err));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-	  <h1>hi darshan</h1>
+        <h1>hi darshan</h1>
       </header>
     </div>
   );

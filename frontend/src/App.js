39f742b70b984875395d625df88dda747e68371c
azure with devops <darshan.az@outlook.com>
2026-05-04 02:28:01 +0530

    added nginx.conf for proxy routing between frontend and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,11 @@ import React, { useEffect } from "react";
 function App() {
 
   useEffect(() => {
-    fetch("http://localhost:8000/health")
-      .then(res => res.json())
-      .then(data => console.log("Backend Response:", data))
-      .catch(err => console.error("Error:", err));
-  }, []);
+  fetch("/api/health")   // ✅ no IP needed
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
+}, []);
 
   return (
     <div className="App">

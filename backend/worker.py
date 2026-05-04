585de4e3fbcb28f1ba43082d086850b3ac145ff6
darshan_koppolu <darshan17146@gmail.com>
2026-05-04 11:08:55 +0000

    added redis container in compose, wrote duplication logic in worker.py using redis

--- backend/worker.py
+++ backend/worker.py
@@ -1,6 +1,10 @@
 from kafka import KafkaConsumer
 import json
 import time
+import redis
+
+# Redis connection
+r = redis.Redis(host="redis", port=6379, decode_responses=True)
 
 consumer = None
 
@@ -17,11 +21,11 @@ def get_consumer():
                 bootstrap_servers="broker:9092",
                 value_deserializer=lambda m: json.loads(m.decode("utf-8")),
                 auto_offset_reset="earliest",
-                group_id="ims-group"   # IMPORTANT
+                group_id="ims-group"
             )
             print("Worker connected to Kafka", flush=True)
             return consumer
-        except Exception as e:
+        except Exception:
             print(f"Worker waiting for Kafka... {i}", flush=True)
             time.sleep(3)
 
@@ -34,7 +38,16 @@ def process():
     print("Worker started listening...", flush=True)
 
     for message in c:
-        print("Received from Kafka:", message.value, flush=True)
+        data = message.value
+        key = f"{data['component_id']}_{data['message']}"
+
+        # 🔥 CHECK REDIS
+        if r.exists(key):
+            print("Duplicate signal ignored:", data, flush=True)
+        else:
+            # first time
+            r.set(key, "active")
+            print("New incident created:", data, flush=True)
 
 
 if __name__ == "__main__":
